b69726de05b649b1b9d30ed472b4ab3bcabef8a9
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-22 12:20:49 -0500

    Initial Commit

---
+++ TrandingCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrandingCards
+//
+//  Created by Pablo Alvarado on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrandingCards/TrandingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrandingCardsApp.swift
+//  TrandingCards
+//
+//  Created by Pablo Alvarado on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TrandingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
